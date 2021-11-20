7e92ca38e5dcb8dcb614eaac385662fe464b80e4
ivan_714 <l1ft@yandex.ru>
2021-11-20 13:04:08 +0400

    Minor fixes.

--- main.py
+++ main.py
@@ -4,41 +4,46 @@ import csv
 import io
 from mysql.connector import connect, Error
 from datetime import datetime
-
 import config
 
+
+# Connect to MySQL server
 try:
-    connection = connect(user=config.USER, password=config.PASSWORD, host=config.HOST, port=config.PORT, database=config.DATABASE)
-    cursor = connection.cursor()
+    db_connection = connect(user=config.USER, password=config.PASSWORD, host=config.HOST, port=config.PORT, database=config.DATABASE)
+    cursor = db_connection.cursor()
 except Error as e:
     print('ERROR: ' + str(e))
     sys.exit(0)
 
+
+# Get the DB fields
 show_table_query = "DESCRIBE clients"
 cursor.execute(show_table_query)
 result = cursor.fetchall()
-database_fields = ""
-formatted_string_for_update_tmp = ""
+db_fields = ""
+formatted_string_for_update_template = ""
 i = 1
 for row in result:
-    database_fields = database_fields + row[0] + ","
+    db_fields = db_fields + row[0] + ","
     i += 1
-    formatted_string_for_update_tmp = formatted_string_for_update_tmp + row[0] + "='{" + str(i-3) + "}',"
-database_fields = database_fields[0:len(database_fields)-1]
-formatted_string_for_update_tmp = formatted_string_for_update_tmp[10:len(formatted_string_for_update_tmp)-1]
+    formatted_string_for_update_template = formatted_string_for_update_template + row[0] + "='{" + str(i-3) + "}',"
 
-#print(result)
+db_fields = db_fields[0:len(db_fields)-1]
+formatted_string_for_update_template = formatted_string_for_update_template[10:len(formatted_string_for_update_template)-1]
 
+
+# Get the GoogleSheet
 url = "https://docs.google.com/spreadsheets/d/{0}/export?format=csv".format(config.FILE_ID)
-request_to_google_sheet = requests.get(url)
-sio = io.StringIO(request_to_google_sheet.content.decode('utf-8'), newline=None)
-reader = csv.reader(sio, dialect=csv.excel)
+sio = io.StringIO(requests.get(url).content.decode('utf-8'), newline=None)
+googlesheet_csv = csv.reader(sio, dialect=csv.excel)
 
-row_count = 0
+rows_in_googlesheet = 0
 
-for row in reader:
-    row_count += 1
-    if row_count > 1: #Skip the first row
+
+# Read the GoogleSheet line by line
+for row in googlesheet_csv:
+    rows_in_googlesheet += 1
+    if rows_in_googlesheet > 1: #Skip the first row
 
         cursor.execute("SELECT COUNT(*) FROM clients WHERE id=" + row[0])
         rowcount = cursor.fetchone()[0]
@@ -74,16 +79,16 @@ for row in reader:
             insert_clients_query = """
             INSERT INTO clients ({0})
             VALUES ({1})
-            """.format(database_fields, data)
+            """.format(db_fields, data)
 
             try:
                 cursor.execute(insert_clients_query)
-                connection.commit()
+                db_connection.commit()
             except Error as e:
                 print('ERROR: ' + str(e))
 
         elif rowcount == 1: #Non-unique record (SQL UPDATE)
-            formatted_string_for_update = formatted_string_for_update_tmp.format(
+            formatted_string_for_update = formatted_string_for_update_template.format(
                 row[1],
                 row[2],
                 str(datetime.strptime(row[3], "%Y-%m-%d %H:%M:%S")),
@@ -118,10 +123,8 @@ for row in reader:
 
             try:
                 cursor.execute(update_clients_query)
-                connection.commit()
+                db_connection.commit()
             except Error as e:
                 print('ERROR: ' + str(e))
 
-    first_row = False
-
-connection.close()
+db_connection.close()
